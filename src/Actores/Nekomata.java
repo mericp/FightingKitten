f9fcde0339db55ede2c897cdd2d5a3fba25fa5f6
mericp <mcpalanques@gmail.com>
2014-07-15 20:04:11 +0200

    STEP 1

--- src/Actores/Nekomata.java
+++ src/Actores/Nekomata.java
@@ -1,9 +1,215 @@
 package Actores;
 
+import ch.qos.logback.classic.Logger;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-
+import com.badlogic.gdx.utils.Array;
+import org.slf4j.LoggerFactory;
 
 public class Nekomata extends Actor
 {
+    private Array<AnimacionConfig> animations = new Array<>();
+    private AnimacionConfig currentAnimation;
+    private int currentAnimationId = -1;
+    private int nextAnimationId = -1;
+    private Animation animation;
+    private TextureRegion originalTexture;
+    private TextureRegion currentFrame;              //Frame actual que se dibujara
+    private int numRows;
+    private int numCols;
+    private float stateTime = 0f;
 
+    private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    public static class AnimacionConfig
+    { 
+        private int id;
+        private TextureRegion[] frames; //Frames that compose the animation.
+        public float durationFrame;     //Duration of each frame.
+        public boolean loop = true;
+        public boolean reverse = true;
+        public boolean uninterrumpible = false; // Can't be interrupted by another animation.
+        public boolean dieAfterAnimationCompleted = false;
+        public boolean isPaused = false;
+
+        public AnimacionConfig(int numFrames, boolean reverse)
+        {
+            if (reverse)
+            {
+                frames = new TextureRegion[numFrames * 2 - 2];
+            }
+            else
+            {
+                frames = new TextureRegion[numFrames];
+            }
+        }
+    }
+
+    public int getNumFramesAnimation()
+    {
+        return animations.get(currentAnimation.id).frames.length;
+    }
+    
+    //originalTexture: The originalTexture region that contains all the frames of the animation.
+    //numRows, numCols: Number of rows and columns that the originalTexture has.
+    //numFrames: The number of frames for this animation.
+    //durationFrame: Duration of each frame.
+    public Nekomata(TextureRegion texture, int numRows, int numCols, int numFramesPerAnimation, float durationFrame)
+    {
+        if (texture == null)
+        {
+            logger.error("The originalTexture does not exist.");
+            return;
+        }
+
+        this.originalTexture = texture;
+        this.numRows = numRows;
+        this.numCols = numCols;
+
+        // Calculate each frame dimensions.
+        int frameWidth = this.originalTexture.getRegionWidth()/this.numCols;
+        int frameHeight = this.originalTexture.getRegionHeight()/this.numRows;
+
+        //Now we set calculated values as Actor's properties.
+        this.setWidth(frameWidth);
+        this.setHeight(frameHeight);
+
+        //Split original originalTexture into individual textures containing one frame each.
+        TextureRegion[][] frames = this.originalTexture.split(frameWidth, frameHeight);
+
+        int numAnimationsPerRow = this.numCols/numFramesPerAnimation;
+        animations = new Array<>(numAnimationsPerRow*this.numRows);
+        
+        int numFrame;
+
+        AnimacionConfig animation = new AnimacionConfig(numFramesPerAnimation, true);
+
+        for (int i = 0; i < this.numRows; i++)
+        {
+            for (int j = 0; j < this.numCols; j++)
+            {
+                numFrame = j % numFramesPerAnimation;
+                animation.frames[numFrame] = frames[i][j];
+
+                //Generate intermediate animations to allow generation of revert effect when needed.
+                if (numFrame > 0 && numFrame < numFramesPerAnimation - 1)
+                {
+                    animation.frames[numFramesPerAnimation - 1 + numFrame] = animation.frames[numFrame];
+                }
+
+                //Once we've generate all the frames, we give it a duration time, we add it and we start creating the next one.
+                if (numFrame >= numFramesPerAnimation-1)
+                {   
+                    animation.durationFrame = durationFrame;
+                    animations.add(animation);
+                    animation = new AnimacionConfig(numFramesPerAnimation, true);
+                }
+            }
+        }
+
+        //Initialize the with default animation:
+        setAnimacion(0, true);
+    }
+
+    public final void setAnimacion (int animationId, boolean forceAnimation)
+    {
+        if (animationId < 0 || animationId >= animations.size)
+        {
+            logger.error("The animation does not exist.");
+            return;
+        }
+
+        if(currentAnimationId == -1)
+        {
+            currentAnimation = animations.get(animationId);
+        }
+        else
+        {
+            currentAnimation = animations.get(currentAnimationId);
+        }
+
+        //If current animation is uninterrumpible, set new animation as next.
+        if (currentAnimation.uninterrumpible && !forceAnimation)
+        {
+            nextAnimationId = animationId;
+            return;
+        }
+
+        //Force animation will reboot animation (without loading it again)
+        if (forceAnimation)
+        {
+            stateTime = 0f;
+        }
+
+        if (currentAnimationId == -1 || animationId != this.currentAnimation.id)
+        {
+            animation = new Animation(currentAnimation.durationFrame, animations.get(animationId).frames);
+            currentAnimationId = animationId;
+            stateTime = 0f;
+        }
+    }
+
+    @Override public void draw (Batch batch, float alpha)
+    {
+        //To modify alpha, we need to first load the color.
+        Color oldColor = batch.getColor();
+        Color newColor = batch.getColor();
+        newColor.a = alpha;
+        batch.setColor(newColor);
+        
+        int numFramesAnimation = getNumFramesAnimation();
+
+        if (!currentAnimation.isPaused)
+        {
+            stateTime += Gdx.graphics.getDeltaTime();
+        }
+
+        if (!currentAnimation.reverse && stateTime > currentAnimation.durationFrame * (numFramesAnimation + 2) / 2)
+        {
+            stateTime = 0f;
+            resetFlags();
+        }
+
+        if (currentAnimation.reverse && currentAnimation.durationFrame > currentAnimation.durationFrame * (numFramesAnimation))
+        {
+            resetFlags();
+        }
+
+        currentFrame = animation.getKeyFrame(stateTime, currentAnimation.loop);
+
+        Vector2 Offset = new Vector2(0,0);
+
+        batch.draw(currentFrame,
+                getX() + Offset.x,
+                getY() + Offset.y,
+                this.getOriginX(),
+                this.getOriginY(),
+                this.getWidth(),
+                this.getHeight(),
+                this.getScaleX(),
+                this.getScaleY(),
+                this.getRotation());
+        
+        //Restore original color.
+        batch.setColor(oldColor);
+    }
+    
+    public void resetFlags()
+    {
+        if(currentAnimation.dieAfterAnimationCompleted)
+        {
+            this.getParent().removeActor(this);
+        }
+
+        if(nextAnimationId >= 0)
+        {
+            setAnimacion(nextAnimationId, true);
+            nextAnimationId = -1;
+        }
+    }
 }
--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,28 +1,30 @@
 
+import Actores.Nekomata;
 import DB.MySettings;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 public class GameScreen implements Screen
 {
-    protected OrthographicCamera camara;
-    protected Stage sMundo;
-
-
+    protected OrthographicCamera camera;
+    protected Stage battlefield;
 
     public GameScreen()
     {
-        camara = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        sMundo = new Stage();
-        sMundo.getViewport().setCamera(camara);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        battlefield = new Stage();
+        battlefield.getViewport().setCamera(camera);
 
+        TextureRegion texture = MySettings.ATLAS_DAO.getAtlasDAO().getTexture("gatito");
+        Nekomata gatito = new Nekomata(texture, 8, 12, 3, 0.20f);
 
+        gatito.setPosition(400,400);
+        battlefield.addActor(gatito);
     }
-
-
 
     @Override public void show()
     {
@@ -34,14 +36,14 @@ public class GameScreen implements Screen
         Gdx.gl.glClearColor(0/2.55f, 0/2.55f, 0/2.55f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        sMundo.act(delta);
-        sMundo.draw();
+        battlefield.act(delta);
+        battlefield.draw();
     }
 
     @Override public void resize(int width, int height)
     {
-        camara.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        sMundo.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        battlefield.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
     }
 
     @Override public void hide()
